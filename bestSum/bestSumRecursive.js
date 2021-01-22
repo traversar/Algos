2f36b54e98823f494889ebb0de853367a3685630
Andrew <travers.drew@gmail.com>
2021-01-21 23:25:10 -0500

    Best Sum Recursive

---
+++ bestSum/bestSumRecursive.js
@@ -0,0 +1,21 @@
+const bestSum = (targetSum, numbers, memo={}) => {
+    if (targetSum in memo) return memo[targetSum];
+    if (targetSum === 0) return [];
+    if (targetSum < 0) return null;
+
+    let shortestCombination = null;
+
+    for (let num of numbers) {
+        const remainder = targetSum - num;
+        const remainderCombination = bestSum(remainder, numbers);
+        if (remainderCombination !== null) {
+            const combination = [...remainderCombination, num];
+            if (shortestCombination === null || combination.length < shortestCombination.length) {
+                shortestCombination = combination;
+            }
+        }
+    }
+
+    memo[targetSum] = shortestCombination;
+    return shortestCombination;
+}
